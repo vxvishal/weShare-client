e4c948341d1a473bba24953d2a14258720b94dbe
Vishal <singhvishalkumar997@gmail.com>
2023-03-01 15:20:47 +0530

    initial commit

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Send from '../components/Send';
+import Receive from '../components/Receive';
+
+export default function Home() {
+  return (
+    <div>
+      <Send />
+      <hr />
+      <Receive />
+    </div>
+  )
+}
